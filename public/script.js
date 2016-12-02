237fef40e44637b40fcfbd54c4d1222cc7dba3ff
Xu <email.xhuang@gmail.com>
2016-12-02 11:42:42 -0500

    Streamline code & added additional array items to server.js

--- public/script.js
+++ public/script.js
@@ -1,13 +1,44 @@
 // script.js, any js file that might perform AJAX operations
-var thought = "";
+
 
 setInterval(function() {
 
-    $.get("/api/randDogThought", function(data){
-      thoughts = data;
+    $.get("/api/randDogThought", function(thoughts){
+        $(".a-thought").text('"'+thoughts+'"');
 
-      $(".a-thought").text('"'+thoughts+'"');
-
+        console.log(thoughts);
     });
 
-}, 3000);
+}, 5000);
+
+
+
+/*NOT QUITE WORKING*/
+
+// var thoughts= "";
+//
+// setInterval(function() {
+//
+//   $.get("/api/randDogThought", function(data){
+//     thoughts = data;
+//   });
+//
+//   $(".a-thought").text('"'+thoughts+'"');
+//
+// }, 1000);
+
+
+
+
+
+// var thoughts = "";
+//
+// $.get("/api/dogThoughts", function(data){
+//   thoughts = data;
+// });
+//
+//
+// setInterval(function(){
+//
+//   $(".a-thought").text('"'+thoughts+'"');
+// }, 1000);
--- server.js
+++ server.js
@@ -2,13 +2,27 @@ var express = require('express');
 var app = express();
 
 
-
 // server.js, any file with a created and configured an express app
 app.use(express.static(__dirname + "/public"))
 
 
-
-var dogThoughts = ["Burrito!", "Ball?", "Ball!", "SQUIRREL!!", "Walk?", "Nap.", "Let me sniff you.", "Rub my belly.", "On the internet, no one knows you're a dog.", "HELLO YES THIS IS DOG", "You gonna finish that?", "I hid under the porch because I love you."];
+var dogThoughts = [
+  "Ball?",
+  "SQUIRREL!!",
+  "On the internet, no one knows you're a dog.",
+  "I ate your shoes because I love you.",
+  "Roses are gray, violets are gray, the mailman is evil.",
+  "What if I never find out who's a good boy?",
+  "Why do humans harvest our poop?",
+  "Master runs slow because he only uses half of his legs.",
+  "If I wasn't around, my human would end up losing dozens of balls every day.",
+  "Master just tied me to a tree. Now I am human, tree is dog.",
+  "I don't want the ball. Then Master says, 'Who wants the ball?' Then I realize that I want the ball.",
+  "How come I can't bring a stick inside, but my human can bring in a whole tree?",
+  "Why does the trash can get fed treats all day?!",
+  "I lick Master every day, but she has never licked me. :(",
+  "Master is afraid of walking alone so he always attaches me to him with a rope. Is this normal?"
+];
 
 var randDogThought = function(){
   return dogThoughts[Math.floor(Math.random() * dogThoughts.length)];
